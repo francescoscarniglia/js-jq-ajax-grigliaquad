a6ad5f8613610361e844e06239dc69664e194f78
francesco scarniglia <francescoscarniglia@gmail.com>
2020-04-27 15:37:30 +0200

    step 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,17 @@
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
 // DevTools console e network sono nostri amici
-
 $(document).ready(function(){
-  console.log('jQ ok');
+
+  var apiFake ='https://flynn.boolean.careers/exercises/api/random/int';
+
+  var boxes = $('.box');
+
+  boxes.click(function(){
+    console.log('Hai pigiato box');
+  });
+
+
+
+
 });//ready
