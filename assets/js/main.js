389d2a8d255b97d45fd4c05f56e07394935ffc65
francesco scarniglia <francescoscarniglia@gmail.com>
2020-04-27 15:22:58 +0200

    start ex
    html/css ok

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+// DevTools console e network sono nostri amici
+
+$(document).ready(function(){
+  console.log('jQ ok');
+});//ready
